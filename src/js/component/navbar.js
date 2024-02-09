1da5a33f951fb739aa4aae44c9080bf3f5699c8b
Kezia Bernard-Nau <147874734+KeziaBernardNau@users.noreply.github.com>
2024-02-09 20:57:40 +0000

    nav bar addition

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,60 +1,27 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-import { Context } from "../store/appContext";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext.js";
 
-export default function Navbar() {
-  const [favoriteItems, setFavoriteItems] = useState([]);
-  const { store, actions } = useContext(Context);
 
-  useEffect(() => {
-    console.log("favorites", store.favorites);
-    setFavoriteItems(store.favorites);
-  }, [store.favorites]);
 
-  return (
-    <nav class="navbar navbar-dark bg-dark">
-      <a class="navbar-brand" href="/">
-        <img src="https://occ-0-2794-1001.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABU1-JRbb0a_bBhPJeiAAGzO4WD7ZOmd0OvHlIhU8ybxBzHxmwuC2qQF4mVAGzQi0ulXc-Q0k_P4KykHJ73o_pcGdAEydHX-Y9VErmh-rvRLj.png?r=2ca" width="100" height="35" className="logo" />
-      </a>
-      <div style={{ marginRight: "7rem" }} className="dropdown">
-        <button
-          className="btn btn-light text-dark dropdown-toggle"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Favorites
-        </button>
-        <ul className="dropdown-menu">
-          {favoriteItems.length ? (
-            favoriteItems.map((item) => {
-              return (
-                <li
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                  }}
-                  className="dropdown-item"
-                >
-                  {item.name}
-                  <button
-                    onClick={() => {
-                      actions.removeFavorite(item);
-                    }}
-                  >
-                    X
-                  </button>{" "}
-                </li>
-              );
-            })
-          ) : (
-            <li>
-              No favorites.
-            </li>
-          )}
-        </ul>
-      </div>
-    </nav>
-  );
+export const Navbar = () => {
+    const { store, actions } = useContext(Context);
+    return (
+        <nav className="navbar navbar-light" style={{ background: "linear-gradient(to right, #000000, #111111)" }}>
+            <button className="btn btn-warning dropdown-toggle ms-auto mx-2 mb-0" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Favorites
+            </button>
+            <div className="dropdown-menu dropdown-menu-end p-2"> 
+                <ul className="px-2">
+                    {store.favorites.map((favorite, index) => (
+                        <li className="navbar d-flex justify-content-between dropdown-menu-item" key={index}>
+                            <div>{favorite.name}</div>
+                            <div className="deleteFav mx-2" onClick={() => actions.deleteFav(favorite.name)}>
+                                <i className="fas fa-trash-alt"></i>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
 };
